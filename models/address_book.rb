a676bd1e2aef2e527597d941030c293354df9e4a
Cody McGowan <cody.mcgowan@gmail.com>
2016-08-12 12:49:56 -0700

    moved entries.csv to the right folder

--- models/address_book.rb
+++ models/address_book.rb
@@ -19,6 +19,16 @@ class AddressBook
     entries.insert(index, Entry.new(name, phone_number, email))
   end
 
+  def import_from_csv(file_name)
+    csv_text = File.read(file_name)
+    csv = CSV.parse(csv_text, headers: true, skip_blanks: true)
+
+    csv.each do |row|
+      row_hash = row.to_hash
+      add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
+    end
+  end
+
   def remove_entry(name, phone_number, email)
     entries.delete(Entry.new(name, phone_number, email))
 
@@ -29,8 +39,5 @@ class AddressBook
     #   end
     # end
     # entries.delete(target_entry)
-  end
-
-  def import_from_csv(file_name)
   end
 end
